eb0845a153426ef9019d2a00160c06809d0d95db
phonchi <phonchi@cad14.(none)>
2016-01-08 10:49:29 +0800

    SomeComment

---
+++ common.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <glib.h>
+#include <openssl/aes.h>
+#include <openssl/sha.h>
+#include <pbc.h>
+
+#include "common.h"
+
+void
+init_cipher( element_t k,unsigned char* key, unsigned char* iv )
+{
+  int key_len;
+
+  key_len = element_length_in_bytes(k) < 11 ? 11 : element_length_in_bytes(k);
+  key = (unsigned char*) malloc(key_len);
+  element_to_bytes(key, k);
+
+  memset(iv, 0, 10);
+}
+
+
+FILE*
+fopen_read_or_die( char* file )
+{
+	FILE* f;
+
+	if( !(f = fopen(file, "r")) )
+		die("can't read file: %s\n", file);
+
+	return f;
+}
+
+FILE*
+fopen_write_or_die( char* file )
+{
+	FILE* f;
+
+	if( !(f = fopen(file, "w")) )
+		die("can't write file: %s\n", file);
+
+	return f;
+}
+
+GByteArray*
+suck_file( char* file )
+{
+	FILE* f;
+	GByteArray* a;
+	struct stat s;
+	a = g_byte_array_new();
+	stat(file, &s);
+	g_byte_array_set_size(a, s.st_size);
+
+	f = fopen_read_or_die(file);
+	fread(a->data, 1, s.st_size, f);
+	fclose(f);
+
+	return a;
+}
+
+char*
+suck_file_str( char* file )
+{
+	GByteArray* a;
+	char* s;
+	unsigned char zero;
+
+	a = suck_file(file);
+	zero = 0;
+	g_byte_array_append(a, &zero, 1);
+	s = (char*) a->data;
+	g_byte_array_free(a, 0);
+
+	return s;
+}
+
+char*
+suck_stdin()
+{
+	GString* s;
+	char* r;
+	int c;
+
+	s = g_string_new("");
+	while( (c = fgetc(stdin)) != EOF )
+		g_string_append_c(s, c);
+
+	r = s->str;
+	g_string_free(s, 0);
+
+	return r;
+}
+
+void
+spit_file( char* file, GByteArray* b, int free )
+{
+	FILE* f;
+
+	f = fopen_write_or_die(file);
+	fwrite(b->data, 1, b->len, f);
+	fclose(f);
+
+	if( free )
+		g_byte_array_free(b, 1);
+}
+
+void read_cpabe_file( char* file,    GByteArray** cph_buf, int* file_len )
+{
+	FILE* f;
+	int i;
+	int len;
+
+	*cph_buf = g_byte_array_new();
+
+	f = fopen_read_or_die(file);
+
+	/* read real file len as 32-bit big endian int */
+	*file_len = 0;
+	for( i = 3; i >= 0; i-- )
+		*file_len |= fgetc(f)<<(i*8);
+	printf("filelength:%d", *file_len);
+	/* read aes buf */
+	/*len = 0;
+	for( i = 3; i >= 0; i-- )
+		len |= fgetc(f)<<(i*8);
+	g_byte_array_set_size(*aes_buf, len);
+	fread((*aes_buf)->data, 1, len, f);
+        */
+	/* read cph buf */
+	len = 0;
+	for( i = 3; i >= 0; i-- )
+		len |= fgetc(f)<<(i*8);
+	g_byte_array_set_size(*cph_buf, len);
+	fread((*cph_buf)->data, 1, len, f);
+	
+	fclose(f);
+}
+
+void
+write_cpabe_file( char* file, GByteArray* cph_buf, int file_len )
+{
+	FILE* f;
+	int i;
+
+	f = fopen_write_or_die(file);
+	printf("filelength:%d", file_len);
+	/* write real file len as 32-bit big endian int */
+	for( i = 3; i >= 0; i-- )
+		fputc((file_len & 0xff<<(i*8))>>(i*8), f);
+
+	/* write aes_buf */
+	/*for( i = 3; i >= 0; i-- )
+		fputc((aes_buf->len & 0xff<<(i*8))>>(i*8), f);
+	fwrite(aes_buf->data, 1, aes_buf->len, f);
+        */
+	/* write cph_buf */
+	for( i = 3; i >= 0; i-- )
+		fputc((cph_buf->len & 0xff<<(i*8))>>(i*8), f);
+	fwrite(cph_buf->data, 1, cph_buf->len, f);
+
+	fclose(f);
+}
+
+void
+die(char* fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	exit(1);
+}
---
+++ enc.c
@@ -0,0 +1,156 @@
+#include <string.h>
+#include <unistd.h>
+#include <glib.h>
+#include <pbc.h>
+#include <pbc_random.h>
+#include <time.h>
+
+#include "bswabe.h"
+#include "common.h"
+#include "policy_lang.h"
+
+char* usage =
+"Usage: cpabe-enc [OPTION ...] PUB_KEY FILE [POLICY]\n"
+"\n"
+"Encrypt FILE under the decryption policy POLICY using public key\n"
+"PUB_KEY. The encrypted file will be written to FILE.cpabe unless\n"
+"the -o option is used. The original file will be removed. If POLICY\n"
+"is not specified, the policy will be read from stdin.\n"
+"\n"
+"Mandatory arguments to long options are mandatory for short options too.\n\n"
+" -h, --help               print this message\n\n"
+" -v, --version            print version information\n\n"
+" -k, --keep-input-file    don't delete original file\n\n"
+" -o, --output FILE        write resulting key to FILE\n\n"
+" -d, --deterministic      use deterministic \"random\" numbers\n"
+"                          (only for debugging)\n\n"
+"";
+
+char* pub_file = 0;
+char* in_file  = 0;
+char* out_file = 0;
+int   keep     = 0;
+
+char* policy = 0;
+
+void
+parse_args( int argc, char** argv )
+{
+	int i;
+
+	for( i = 1; i < argc; i++ )
+		if(      !strcmp(argv[i], "-h") || !strcmp(argv[i], "--help") )
+		{
+			printf("%s", usage);
+			exit(0);
+		}
+		else if( !strcmp(argv[i], "-v") || !strcmp(argv[i], "--version") )
+		{
+			printf(CPABE_VERSION, "-enc");
+			exit(0);
+		}
+		else if( !strcmp(argv[i], "-k") || !strcmp(argv[i], "--keep-input-file") )
+		{
+			keep = 1;
+		}
+		else if( !strcmp(argv[i], "-o") || !strcmp(argv[i], "--output") )
+		{
+			if( ++i >= argc )
+				die(usage);
+			else
+				out_file = argv[i];
+		}
+		else if( !strcmp(argv[i], "-d") || !strcmp(argv[i], "--deterministic") )
+		{
+			pbc_random_set_deterministic(0);
+		}
+		else if( !pub_file )
+		{
+			pub_file = argv[i];
+		}
+		else if( !in_file )
+		{
+			in_file = argv[i];
+		}
+		else if( !policy )
+		{
+			policy = parse_policy_lang(argv[i]);
+		}
+		else
+			die(usage);
+
+	if( !pub_file || !in_file )
+		die(usage);
+
+	if( !out_file )
+		out_file = g_strdup_printf("%s.cpabe", in_file);
+
+	if( !policy )
+		policy = parse_policy_lang(suck_stdin());
+}
+
+int
+main( int argc, char** argv )
+{
+	bswabe_pub_t* pub;
+	bswabe_cph_t* cph;
+	int file_len;
+	GByteArray* plt;
+	GByteArray* cph_buf;
+	element_t m;
+        unsigned char* key;
+        unsigned char iv[10];
+	FILE *fp;
+        int i;
+
+
+	clock_t start, end;
+	float time_result;
+	
+        parse_args(argc, argv);
+	pub = bswabe_pub_unserialize(suck_file(pub_file), 1);
+	//Start timer
+	start=clock();
+ 	if( !(cph = bswabe_enc(pub, m, policy)) )
+		die("%s", bswabe_error());
+	free(policy);
+	cph_buf = bswabe_cph_serialize(cph);
+	bswabe_cph_free(cph);
+
+	plt = suck_file(in_file);
+	file_len = plt->len;
+	g_byte_array_free(plt, 1);
+	
+        init_cipher(m, key, iv);
+        /*for(i=0; i<16; i++)
+            printf("%02X", key[i]);
+        for(i=0; i<16; i++)
+            printf("%02X", iv[i]);
+        */
+        fp = fopen("keyfile_enc","w");
+        if(fp == NULL){
+            printf("open fail");
+            return 1;
+        }else{
+            for(i=0; i<10; i++)
+                fprintf(fp,"%02X", key[i]);
+            fprintf(fp,"\n");
+            for(i=0; i<10; i++)                    
+                fprintf(fp,"%02X", iv[i]);             
+        }
+        fclose(fp);
+
+
+	write_cpabe_file(out_file, cph_buf, file_len);
+	//End Timer, display output
+	end=clock();
+	time_result = (float) (end-start)/(float) CLOCKS_PER_SEC;
+	//printf("Computation took %f seconds\n",time_result);
+	element_clear(m);
+	g_byte_array_free(cph_buf, 1);
+
+	if( !keep )
+		unlink(in_file);
+
+	return 0;
+}
